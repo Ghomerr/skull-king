4fc344f2311347915df99237e508fe49e349f29a
Ghomerr <ghomerr.games@gmail.com>
2025-01-22 21:44:43 +0100

    Updates on lobby handling (WIP)

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const SERVER_PORT = 8181;
 const MAX_ROOMS = 10;
 const MIN_PLAYERS = 2;
 const MAX_PLAYERS = 7;
+const STATUS = {
+    IN_LOBBY_WAITING: 1,
+    IN_LOBBY_FULL: 2
+};
 
 // Start server and expose main page
 app.get('/', (req, res) => {
@@ -35,7 +39,7 @@ let roomsCount = 0;
 io.on('connection', (Socket) => {
     console.log('New player on the main page. Socket.id=', Socket.id);
 
-     // Prepare rooms list result
+     // Prepare the rooms list result
      const roomsList = [];
      for (const [id, room] of Object.entries(ROOMS)) {
          roomsList.push({
@@ -57,10 +61,74 @@ io.on('connection', (Socket) => {
         // #2 Send first response to client : random room id
         Socket.emit('random-room-id', Utils.randomRoomId());
     });
+
+    // A player has started a lobby instance waiting other players
+    Socket.on('start-lobby', (lobbyData) => {
+        console.log('Handling a start lobby request', Socket.id, lobbyData);
+
+        // Save socket and room ids
+        SOCKETS[Socket.id] = {
+            userId: lobbyData.userId,
+            roomId: lobbyData.roomId
+        };
+
+        // For a new room, create its new instance
+        if (!ROOMS[lobbyData.roomId]) {
+            // Check the max rooms value
+            if (Object.keys(ROOMS).length < MAX_ROOMS) {
+
+                const newRoom = {
+                    id: lobbyData.roomId,
+                    status: STATUS.IN_LOBBY_WAITING,
+                    owner: lobbyData.userId,
+                    password: lobbyData.password,
+                    users: [],
+                };
+
+                ROOMS[lobbyData.roomId] = newRoom;
+                Game.initializeRoomGameData(newRoom);
+
+            } else {
+                Socket.emit('player-error', { type: 'maximum-rooms-count', data: MAX_ROOMS });
+                return;
+            }
+        }
+
+        // Add the new player in the room
+        const room = ROOMS[lobbyData.roomId];
+        if (room.status === STATUS.IN_LOBBY_WAITING) {
+            if (room.users.length < MAX_PLAYERS) {
+                if (!room.password || lobbyData.password === room.password) {
+                    if (!room.users.some(u => u.id === lobbyData.userId)) {
+
+                        const newUser = {
+                            id: lobbyData.userId
+                        };
+                        room.users.push(newUser);
+                        Socket.join(lobbyData.roomId);
+                        if (room.users.length === MAX_PLAYERS) {
+                            room.status = STATUS.IN_LOBBY_FULL;
+                        } 
+                        io.to(lobbyData.roomId).emit('players-list-changed', room); // TODO SIMPLIFIED LIST !
+
+                    } else {
+                        socket.emit('player-error', { type: 'player-already-exists', data: lobbyData.userId });
+                    }
+                } else {
+                    socket.emit('player-error', { type: 'password-error', data: lobbyData.roomId });
+                }
+            } else {
+                Socket.emit('player-error', { type: 'full-lobby', data: lobbyData.roomId });
+            }
+
+        } else {
+            Socket.emit('player-error', { type: 'already-in-game' });
+        }
+    });
 });
 
 // Server start
 const port = process.env.PORT || SERVER_PORT;
 http.listen(port, () => {
-    console.log('Server listening on port', port);
+    console.log('Server listening on http://localhost:' + port);
 });
--- static/scripts/client.js
+++ static/scripts/client.js
@@ -1,10 +1,68 @@
 const Socket = io();
+const Global = {};
 const Lobby = {};
+const Player = {};
 
 $(document).ready(() => {
+
+    //------------------------------------------------------------//
+
+    Global.$simpleDialog = $('#simple-dialog');
+    Global.openSimpleDialog = ($dialog, title, text, width) => {
+        $dialog.dialog('option', 'title', title);
+        if (width) {
+            $dialog.dialog('option', 'width', width);
+        } else {
+            // Reset default width
+            $dialog.dialog('option', 'width', 300);
+        }
+        $dialog.find('#dialog-text').text('').append(text);
+        $dialog.dialog('open');
+    };
     
+    // Handle when player makes an error
+    Socket.on('player-error', (error) => {
+        switch (error.type) {
+            case 'maximum-rooms-count':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur', 'Le nombre de rooms maximum a été atteint : ' + error.data);
+                break;
+            case 'player-already-exists':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur', 'Le nom de joueur choisi est déjà pris dans cette room : ' + error.data);
+                break;
+            case 'already-in-game':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur', 'Vous ne pouvez pas rejoindre une partie déjà en cours !');
+                break;
+            case 'full-lobby':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur', 'Vous ne pouvez pas rejoindre la salle de jeu ' + error.data + ', car elle est déjà complète.');
+                break;
+            case 'password-error':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur', 'Le mot de passe de la Salle de jeu ' + error.data + ' est incorrect.');
+                break;
+            case 'wrong-type':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Attention', 'Vous devez jouer une carte ' + error.data + ' !');
+                break;
+            case 'cannot-play-this-card':
+                Global.openSimpleDialog(Global.$simpleDialog, 'Attention', 'Vous ne pouvez pas jouer cette carte : ' + error.data);
+                break;
+            default:
+                Global.openSimpleDialog(Global.$simpleDialog, 'Erreur!', 'Erreur inconnue: ' + error.type + ' ' + error.data);
+        }
+    });
+
+    //------------------------------------------------------------//
+
     Lobby.$randomRoomIdBtn = $('#random-room-id-btn');
     Lobby.$roomIdInput = $('#room-id');
+    Lobby.$roomPasswordLink = $('#room-password-link');
+    Lobby.$roomPasswordContainer = $('#room-password-container');
+    Lobby.$lobbyInputsContainer = $('#lobby-inputs'),
+    Lobby.$requiredInputs = $('#room-id, #user-id');
+    Lobby.inputs = {
+        $roomId: $('#room-id'),
+        $userId: $('#user-id'),
+        $roomPassword: $('#room-password')
+    };
+    Lobby.$submitButton = $('#lobby-btn');
 
     Lobby.$randomRoomIdBtn.click(() => {
         Socket.emit('get-random-room-id');
@@ -13,9 +71,35 @@ $(document).ready(() => {
     Socket.on('random-room-id', (roomId) => {
         Lobby.$roomIdInput.val(roomId);
     });
+
+    // Handle click on private party link
+    Lobby.$roomPasswordLink.click(() => {
+        Lobby.$roomPasswordLink.hide();
+        Lobby.$roomPasswordContainer.show();
+    });
+
+    // Handle all valid input to enable connection button
+    Lobby.$requiredInputs.on('keyup change', () => {
+        Lobby.$submitButton.prop('disabled', !Lobby.inputs.$roomId.val() || !Lobby.inputs.$userId.val());
+    });
+
+    // Handle lobby button click
+    Lobby.$submitButton.click(() => {
+        const lobbyData = {
+            userId: Lobby.inputs.$userId.val(),
+            roomId: Lobby.inputs.$roomId.val(),
+            password: Lobby.inputs.$roomPassword.val()
+        };
+        Player.id = lobbyData.userId;
+        console.log('Sending start lobby event', lobbyData);
+
+        // Send lobby event to server
+        Socket.emit('start-lobby', lobbyData);
+    });
 });
 
 // Events
+
 // First event received from server.js
 Socket.on('connected', (data) => {
     console.log('Client connected with server', data);
@@ -53,5 +137,42 @@ Socket.on('connected', (data) => {
             //$roomsListContent.append(roomsListText);
         }
         //$roomsList.show();
+    }
+});
+
+// Handle user joined lobby
+Socket.on('players-list-changed', (room) => {
+    console.log('User has entered in lobby', room);
+    Player.roomId = room.id;
+    Lobby.$lobbyInputsContainer.hide();
+
+    // TODO NEXT ... 
+    $roomsList.hide();
+    $playersList.show();
+    $playersList.find('.room-id-title').text(room.id);
+    const $lobbyPlayersList = $playersList.find('.lobby-players-list');
+    $lobbyPlayersList.text('');
+
+    // Display players in the lobby
+    room.users.forEach(user => {
+        let username = user.id;
+        if (user.id === room.owner) {
+            username = '<i class="fas fa-crown"></i>' + username;
+        }
+        if (user.id === myUserId) {
+            username = '<strong>' + username + '</strong>';
+        }
+        $lobbyPlayersList.append('<div class="user">' + username + '</div>');
+    });
+
+    // Start conditions
+    $playerCounter.text(room.users.length);
+    if (room.owner === myUserId) {
+        $startBtn.show();
+        $startBtn.prop('disabled', room.users.length < 3 || room.users.length > 5);
+        if (room.password) {
+            $infoPassword.text('Mot de passe : ' + room.password);
+            $infoPassword.show();
+        }
     }
 });
